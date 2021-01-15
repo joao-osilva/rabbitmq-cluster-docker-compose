0e4c8e583f247d9d56bcaa981af28d35a8f18be3
Steve Kieu <msh.computing@gmail.com>
2021-01-15 17:04:22 +1000

    Test haproxy SSL termination
    - modify compose file to allow dynamic config and key
    - support listen on SSL
    - add test go client to test teh SSL connection

---
+++ up-cluster.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker exec -ti rabbitmq-node-2 bash -c "rabbitmqctl stop_app"
+docker exec -ti rabbitmq-node-2 bash -c "rabbitmqctl join_cluster rabbit@rabbitmq-node-1"
+docker exec -ti rabbitmq-node-2 bash -c "rabbitmqctl start_app"
+
+docker exec -ti rabbitmq-node-3 bash -c "rabbitmqctl stop_app"
+docker exec -ti rabbitmq-node-3 bash -c "rabbitmqctl join_cluster rabbit@rabbitmq-node-1"
+docker exec -ti rabbitmq-node-3 bash -c "rabbitmqctl start_app"
